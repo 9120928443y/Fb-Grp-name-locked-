648bbada5b982a7a57e7e6f3da6e6b627f4e4277
9120928443y <9120928443y@gmail.com>
2026-08-29 00:17:20 +0530

    Set GROUP_THREAD_ID to the correct value
    Updated GROUP_THREAD_ID with the actual group ID.

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ try {
 }
 
 // ✅ Group Info (change these)
-const GROUP_THREAD_ID =      // G1728931761745259roup ka ID
+const GROUP_THREAD_ID =     1728931761745259
 const LOCKED_GROUP_NAME = "     JAAT KI MAA KI KALI CHUTTT 
 // ✅ Express Server to keep bot alive (for Render or UptimeRobot)
 const app = express();
